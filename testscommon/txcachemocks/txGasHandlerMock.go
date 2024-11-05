09f30ed055218e9c1290aa2b00c038e702dd37e9
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-11-05 21:10:25 +0200

    Fix after self-review.

--- testscommon/txcachemocks/txGasHandlerMock.go
+++ testscommon/txcachemocks/txGasHandlerMock.go
@@ -7,19 +7,10 @@ import (
 	"github.com/multiversx/mx-chain-core-go/data"
 )
 
-// TxGasHandler -
-type TxGasHandler interface {
-	MinGasPrice() uint64
-	MaxGasLimitPerTx() uint64
-	ComputeTxFee(tx data.TransactionWithFeeHandler) *big.Int
-	IsInterfaceNil() bool
-}
-
 // TxGasHandlerMock -
 type TxGasHandlerMock struct {
 	minGasLimit      uint64
 	minGasPrice      uint64
-	maxGasLimitPerTx uint64
 	gasPerDataByte   uint64
 	gasPriceModifier float64
 }
@@ -29,38 +20,15 @@ func NewTxGasHandlerMock() *TxGasHandlerMock {
 	return &TxGasHandlerMock{
 		minGasLimit:      50000,
 		minGasPrice:      1000000000,
-		maxGasLimitPerTx: 600000000,
 		gasPerDataByte:   1500,
 		gasPriceModifier: 0.01,
 	}
-}
-
-// WithMinGasLimit -
-func (ghm *TxGasHandlerMock) WithMinGasLimit(minGasLimit uint64) *TxGasHandlerMock {
-	ghm.minGasLimit = minGasLimit
-	return ghm
-}
-
-// WithMinGasPrice -
-func (ghm *TxGasHandlerMock) WithMinGasPrice(minGasPrice uint64) *TxGasHandlerMock {
-	ghm.minGasPrice = minGasPrice
-	return ghm
 }
 
 // WithGasPriceModifier -
 func (ghm *TxGasHandlerMock) WithGasPriceModifier(gasPriceModifier float64) *TxGasHandlerMock {
 	ghm.gasPriceModifier = gasPriceModifier
 	return ghm
-}
-
-// MinGasPrice -
-func (ghm *TxGasHandlerMock) MinGasPrice() uint64 {
-	return ghm.minGasPrice
-}
-
-// MaxGasLimitPerTx -
-func (ghm *TxGasHandlerMock) MaxGasLimitPerTx() uint64 {
-	return ghm.maxGasLimitPerTx
 }
 
 // ComputeTxFee -
