0f01e5c5f3c863ec2326235625400705ba1b4bff
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-11-14 14:26:18 +0200

    Additional tests.

--- testscommon/txcachemocks/accountNonceProviderMock.go
+++ testscommon/txcachemocks/accountNonceProviderMock.go
@@ -4,25 +4,34 @@ import (
 	"errors"
 )
 
-type accountNonceProviderMock struct {
+// AccountNonceProviderMock -
+type AccountNonceProviderMock struct {
+	NoncesByAddress       map[string]uint64
 	GetAccountNonceCalled func(address []byte) (uint64, error)
 }
 
 // NewAccountNonceProviderMock -
-func NewAccountNonceProviderMock() *accountNonceProviderMock {
-	return &accountNonceProviderMock{}
+func NewAccountNonceProviderMock() *AccountNonceProviderMock {
+	return &AccountNonceProviderMock{
+		NoncesByAddress: make(map[string]uint64),
+	}
 }
 
 // GetAccountNonce -
-func (stub *accountNonceProviderMock) GetAccountNonce(address []byte) (uint64, error) {
+func (stub *AccountNonceProviderMock) GetAccountNonce(address []byte) (uint64, error) {
 	if stub.GetAccountNonceCalled != nil {
 		return stub.GetAccountNonceCalled(address)
 	}
 
-	return 0, errors.New("GetAccountNonceCalled is not set")
+	nonce, ok := stub.NoncesByAddress[string(address)]
+	if !ok {
+		return 0, errors.New("cannot get nonce")
+	}
+
+	return nonce, nil
 }
 
 // IsInterfaceNil -
-func (stub *accountNonceProviderMock) IsInterfaceNil() bool {
+func (stub *AccountNonceProviderMock) IsInterfaceNil() bool {
 	return stub == nil
 }
