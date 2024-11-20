03c5adb907fcc58e4388aa32d6aa136005d3e054
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-11-20 11:48:26 +0200

    Better handling of not-executable transactions.

--- types/accountState.go
+++ types/accountState.go
@@ -4,6 +4,7 @@ import "math/big"
 
 // AccountState represents the state of an account, as seen by the mempool
 type AccountState struct {
-	Nonce   uint64
-	Balance *big.Int
+	Nonce    uint64
+	Balance  *big.Int
+	Guardian []byte
 }
