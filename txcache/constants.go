acfcb55b960e840e1b17d735a6ad03ab95947950
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-12-19 10:12:27 +0200

    Move txcache functionality to the node.

--- txcache/constants.go
+++
@@ -1,5 +0,0 @@
-package txcache
-
-const diagnosisMaxTransactionsToDisplay = 10000
-const initialCapacityOfSelectionSlice = 30000
-const selectionLoopDurationCheckInterval = 10
--- txcache/errors.go
+++
@@ -1,8 +0,0 @@
-package txcache
-
-import "errors"
-
-var errNilMempoolHost = errors.New("nil mempool host")
-var errNilSelectionSession = errors.New("nil selection session")
-var errItemAlreadyInCache = errors.New("item already in cache")
-var errEmptyBunchOfTransactions = errors.New("empty bunch of transactions")
